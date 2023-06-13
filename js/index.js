127a5b4bbce4966d766928293a54aa4dd1761794
Sabina Kazantseva <sabinakazantceva@gmail.com>
2023-06-12 22:48:40 -0700

    Handle message form submit.

--- js/index.js
+++ js/index.js
@@ -1,17 +1,31 @@
+// Add elements to the footer
 const today = new Date();
 const thisYear = today.getFullYear();
 const footer = document.querySelector('footer');
 const copyright = document.createElement('p');
-let myName = "Sabina Kazantseva";
-const currentYear = new Date().getFullYear();
-copyright.innerHTML =  "\u00A9"  + myName + " " + currentYear;
+const myName = "Sabina Kazantseva";
+copyright.innerHTML = "\u00A9" + myName + " " + thisYear;
 footer.appendChild(copyright);
+
+// Add skills to the skills sections
 const skills = ["HTML", "CSS", "JavaScript", "Git"];
 const skillsSection = document.querySelector('#skills');
 const skillsList = skillsSection.querySelector('ul');
 for (let i = 0; i < skills.length; i++) {
-    const skill = document.createElement('li');
-    skill.innerText = skills[i];
-    skillsList.appendChild(skill);
-  }
+  const skill = document.createElement('li');
+  skill.innerText = skills[i];
+  skillsList.appendChild(skill);
+}
 
+// Message form
+console.log("blablabla");
+const messageForm = document.getElementsByName("leave_message")[0];
+messageForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const usersName = event.target.usersName.value;
+  const usersEmail = event.target.usersEmail.value;
+  const usersMessage = event.target.usersMessage.value;
+  console.log("usersName ===> ", usersName);
+  console.log("usersEmail ===> ", usersEmail);
+  console.log("usersMessage ===> ", usersMessage);
+});
